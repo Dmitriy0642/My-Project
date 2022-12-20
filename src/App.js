55a9d3f187d676731550a18b46e660d34f59cfe7
Dmitriy <dmitriy.linnik1337@mail.ru>
2022-12-20 18:27:16 +0300

    end-develop-hoodies

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import MainPage from "./layouts/mainPage";
 import { Route, Switch } from "react-router-dom";
 import SwetShirt from "./components/swetshirt/swetShirt";
 import ReviewSweatshorts from "./components/swetshirt/reviewSweatshirts";
+import Hoodies from "./components/hoodies/hoodies";
+import HoodiesReview from "./components/hoodies/hoodiesReview";
 
 function App() {
   return (
     <div>
       <Switch>
+        <Route path="/hoodies/:postId" component={HoodiesReview} />
+        <Route path="/hoodies" component={Hoodies} />
         <Route path="/swetshirt/:postId" component={ReviewSweatshorts} />
         <Route path="/swetshirt" component={SwetShirt} />
         <Route path="/" component={MainPage} />
--- src/components/form/reusableForm.jsx
+++ src/components/form/reusableForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "../styles.component/reusableForm.module.css";
 import { Link } from "react-router-dom";
 
@@ -14,7 +14,7 @@ const FormCatalog = ({ data }) => {
           firm={item.firm}
           className={styles.main_div}
         >
-          <Link to={`/swetshirt/${item._id}`}>
+          <Link to={`/hoodies/${item._id}`}>
             <img
               src={item.img[0]}
               alt=""
@@ -22,7 +22,7 @@ const FormCatalog = ({ data }) => {
               id={item._id}
             />
           </Link>
-          <Link to={`/swetshirt/${item._id}`}>
+          <Link to={`/hoodies/${item._id}`}>
             <h2 className={styles.title}>{item.name}</h2>
           </Link>
         </div>
